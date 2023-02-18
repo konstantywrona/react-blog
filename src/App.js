100b78d45a353cf9a7f29d6a67205653c630015f
Konstanty Wrona <koswro@gmail.com>
2023-02-19 00:42:16 +0100

    Add basic app structure 15.2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/pages/home/Home';
+import SinglePost from './components/pages/singlePost/SinglePost';
+import PostAdd from './components/pages/postAdd/PostAdd';
+import PostEdit from './components/pages/postEdit/PostEdit';
+import About from './components/pages/about/About';
+import NotFound from './components/pages/notFound/NotFound';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/post/:id" element={<SinglePost />} />
+      <Route path="/post/add" element={<PostAdd />} />
+      <Route path="/post/edit/:id" element={<PostEdit />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
