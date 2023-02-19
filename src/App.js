d80ba2ee245a97a2675098ac54545f7fcf7370a3
Konstanty Wrona <koswro@gmail.com>
2023-02-19 01:22:51 +0100

    Add Header NavBar Footer 15.3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import { Route, Routes } from 'react-router-dom';
-import Home from './components/pages/home/Home';
-import SinglePost from './components/pages/singlePost/SinglePost';
-import PostAdd from './components/pages/postAdd/PostAdd';
-import PostEdit from './components/pages/postEdit/PostEdit';
-import About from './components/pages/about/About';
-import NotFound from './components/pages/notFound/NotFound';
+import Home from './components/pages/Home/Home';
+import SinglePost from './components/pages/SinglePost/SinglePost';
+import PostAdd from './components/pages/PostAdd/PostAdd';
+import PostEdit from './components/pages/PostEdit/PostEdit';
+import About from './components/pages/About/About';
+import NotFound from './components/pages/NotFound/NotFound';
+import Header from './components/views/Header/Header';
+import Footer from './components/views/Footer/Footer';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/post/:id" element={<SinglePost />} />
-      <Route path="/post/add" element={<PostAdd />} />
-      <Route path="/post/edit/:id" element={<PostEdit />} />
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <Container>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/post/:id" element={<SinglePost />} />
+        <Route path="/post/add" element={<PostAdd />} />
+        <Route path="/post/edit/:id" element={<PostEdit />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </Container>
   );
 }
 
