ae6ff334e38b5c7206650fbb3428217b07f3d664
Konstanty Wrona <koswro@gmail.com>
2023-03-10 00:26:27 +0100

    Add adding post 16.6

---
+++ src/components/features/AddPostForm/AddPostForm.js
@@ -0,0 +1,17 @@
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { addPost } from '../../../redux/postsRedux';
+import PostForm from '../PostForm/PostForm';
+
+const AddPostForm = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleSubmit = (post) => {
+    dispatch(addPost(post));
+    navigate('/');
+  };
+  return <PostForm action={handleSubmit} actionText="Add post" />;
+};
+
+export default AddPostForm;
