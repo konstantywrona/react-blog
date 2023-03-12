3ec34f5f86c6729249ca90fb9fa0b9578d91d436
Konstanty Wrona <koswro@gmail.com>
2023-03-13 00:24:21 +0100

    Add EditPostForm 16.7

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home/Home';
 import SinglePost from './components/pages/SinglePost/SinglePost';
-import PostAdd from './components/pages/PostAdd/PostAdd';
-import PostEdit from './components/pages/PostEdit/PostEdit';
 import About from './components/pages/About/About';
 import NotFound from './components/pages/NotFound/NotFound';
 import Header from './components/views/Header/Header';
 import Footer from './components/views/Footer/Footer';
 import { Container } from 'react-bootstrap';
+import AddPostForm from './components/features/AddPostForm/AddPostForm';
+import EditPostForm from './components/features/EditPostForm/EditPostForm';
 
 function App() {
   return (
@@ -16,8 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post/:id" element={<SinglePost />} />
-        <Route path="/post/add" element={<PostAdd />} />
-        <Route path="/post/edit/:id" element={<PostEdit />} />
+        <Route path="/post/add" element={<AddPostForm />} />
+        <Route path="/post/edit/:id" element={<EditPostForm />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
